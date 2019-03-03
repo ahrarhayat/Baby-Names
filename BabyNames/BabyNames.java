9c2c4fe766da5e4d0fae3284ae38a74911f2c7ae
ahrarhayat <ahrarhayat@gmail.com>
2019-03-03 18:34:02 +0600

    [ADD] Added new methods

--- BabyNames/BabyNames.java
+++ BabyNames/BabyNames.java
@@ -60,8 +60,139 @@ public class BabyNames {
  FileResource fr = new FileResource("FakeData/testing/yob2014short.csv");
  totalBirths(fr);
    }
-   public int getRank(String year, String name, String gender)
+   public int getRank(int year, String name, String gender)
    {
-       return 0;
+       int rank =1;
+       FileResource fr = new FileResource("FakeData/testing/"+"yob"+year+"short.csv");
+       for (CSVRecord rec : fr.getCSVParser(false))
+       {
+           if(rec.get(1).equals(gender))
+           {
+           if(rec.get(0).equals(name))
+           {
+               return rank;
+           }
+           else{
+               rank = rank +1;
+            }
+           
+       }
+     
+    }
+       return -1;
+       //This method could have another implementation
    }
+   public void testGetRank()
+   {
+       int rank=0;
+       String name="Emma";
+       String gender="F";
+       int year = 2014;
+       rank = getRank(year,name,gender);
+       System.out.println("Rank of "+name+" is "+rank);
+   }
+   public String getName(int year,int rank,String gender )
+   {
+      FileResource fr = new FileResource("FakeData/testing/"+"yob"+year+"short.csv");
+      int count = 0;
+    for(CSVRecord rec:fr.getCSVParser(false))
+    {  
+      
+      if(rec.get(1).equals(gender))
+    { 
+        count=count+1;
+        if(count==rank)
+        {
+            return rec.get(0);
+        }
+        
+    }
+
+   }
+   return "NO NAME";
 }
+  public void testGetName()
+  {
+      int year=2014;
+      int rank=2;
+      String gender="F";
+      String name = getName( year, rank, gender );
+      System.out.println("The name at rank "+rank+" is "+name);
+  }
+  public void whatIsNameInYear(String name, String gender, int year, int newYear)
+  {
+      int rank= getRank(year,name,gender);
+      String nameNewYear= getName(newYear,rank,gender);
+      System.out.println(name+ " born in " + year +
+                         " would be " + nameNewYear+ " if he/she was born in "
+                         + newYear);
+  }
+  public void testWhatIsNameInYear()
+  {
+      String name="Isabella";
+      int year = 2012;
+      String gender ="F";
+      int newYear=2014;
+      whatIsNameInYear(name,gender,year,newYear);
+  }
+  public int yearOfHighestRank(String name, String gender)
+  {
+      DirectoryResource dr = new DirectoryResource();
+    
+      String year = "";
+      int yeari=0;
+      int rankTemp=-1;
+      for(File f : dr.selectedFiles())
+      {
+       FileResource fr = new FileResource(f);
+       int count=0;
+       int rank=1;
+       for (CSVRecord rec : fr.getCSVParser(false))
+       {     
+           if(rec.get(1).equals(gender) && count==0 && rec.get(0).equals(name))
+           {
+               System.out.println("Rank on file is "+rank); 
+               count=1;
+           }
+           else{
+               rank = rank +1;
+            }
+        
+}
+      if(rankTemp==-1)
+      {
+          rankTemp=rank+1;
+      }
+      if(rank<rankTemp)
+       {
+           rankTemp=rank;
+           year= f.getName();
+           System.out.println(year);
+       }
+
+}
+ System.out.println(year.substring(3,7));
+ yeari=Integer.parseInt(year.substring(3,7));
+      if(yeari>0)
+      {
+          return yeari;
+      }
+      else{
+    return -1;
+}
+//There is a bug with this method, it returns value for both male and female genders
+}
+
+
+
+  public void testYearOfHighestRank()
+  {
+      String name="Isabella";
+      String gender="F";
+      int x =yearOfHighestRank( name, gender);
+      System.out.println(name+ " is ranked the highest in the year "+x);
+      
+    }
+    
+}
+
